dac5e19ed4d66b1c302141036b3aa17c3393038d
Carlos Contreiras <carlos.darth@gmail.com>
2014-11-18 00:15:54 -0700

    Monday Talks Material :pizza:

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var led = function() {
+	this.colors = ["#2ecc71", 
+	"#e74c3c", 
+	"#f1c40f", 
+	"#9b59b6",
+	"#3498db",
+	"#e67e22"];
+	this.StartBlinking();
+}
+
+led.prototype.RandomColor  = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+led.prototype.StartBlinking = function (){
+	var that = this;
+	setInterval(function() {
+
+    $('.led').css( "background-color", 
+    	that.colors[that.RandomColor(0,that.colors.length)]);
+
+}, 50); //Aqui se modifica el valor de refresco
+	
+}
+
+$(function () {
+	led = new led();
+});
