7981d907217464ff7205ba2ed674d0a2bbdc4701
DarthCharles <carlos.darth@gmail.com>
2014-11-19 17:26:51 -0700

    Commit para expo

---
+++ Java/Monday_Talks/src/monday_talks/Monday_Talks.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monday_talks;
+
+/**
+ *
+ * @author Admin
+ */
+public class Monday_Talks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Peliculas Peliculas_Chidas = new Peliculas();
+     
+     Peliculas_Chidas = null;
+     
+        for (int i = 0; i < Peliculas_Chidas.getPeliculas().size(); i++) {
+            System.out.println(Peliculas_Chidas.getPeliculas().get(i + 1));
+        }
+     
+        Peliculas_Chidas.addPelicula("The Others");
+        Peliculas_Chidas.deletePelicula("V for Vendetta");
+  
+        int random =  (int)(Math.random() * Peliculas_Chidas.getPeliculas().size());
+        Sytem.out.println("La pelicula más chida es " + Peliculas_Chidas.getPeliculas().get(random) );
+        
+ 
+    
+    }
+    
+}
---
+++ Java/Monday_Talks/src/monday_talks/Peliculas.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monday_talks;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class Peliculas {
+
+    private ArrayList<String> Peliculas;
+
+    public Peliculas() {
+        initPeliculas();
+    }
+
+    private void initPeliculas() {
+        this.Peliculas = new ArrayList<String>();
+        Peliculas.add("Star Wars Episode III");
+        Peliculas.add("V for Vendetta");
+        Peliculas.add("Pulp Fiction");
+        Peliculas.add("The Girl of the Dragon Tatoo");
+        Peliculas.add("X-Men");
+        Peliculas.add("Scarface");
+        Peliculas.add("American History X");
+        Peliculas.add("Fight Club");
+    }
+
+    public ArrayList<String> getPeliculas() {
+        return Peliculas;
+    }
+
+    public void addPelicula(String name) {
+//        Method implementation goes HERE
+    }
+
+    public void deletePelicula(String name) {
+//        Method implementation goes HERE
+    }
+}
