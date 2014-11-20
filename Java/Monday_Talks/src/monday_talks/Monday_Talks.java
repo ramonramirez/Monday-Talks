bb6ee5018af43d9298b5443c1d5adbb331956dab
ramonramirez <ramon.ramirezp@outlok.com>
2014-11-20 12:39:21 -0700

    Modifico sitaxis en system.out.println
    Para la impresion de la pelicula mas chida es

--- Java/Monday_Talks/src/monday_talks/Monday_Talks.java
+++ Java/Monday_Talks/src/monday_talks/Monday_Talks.java
@@ -15,19 +15,19 @@ public class Monday_Talks {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//     Peliculas Peliculas_Chidas = new Peliculas();
-//     
-//     Peliculas_Chidas = null;
-//     
-//        for (int i = 0; i < Peliculas_Chidas.getPeliculas().size(); i++) {
-//            System.out.println(Peliculas_Chidas.getPeliculas().get(i + 1));
-//        }
-//     
-//        Peliculas_Chidas.addPelicula("The Others");
-//        Peliculas_Chidas.deletePelicula("V for Vendetta");
-//  
-//        int random =  (int)(Math.random() * Peliculas_Chidas.getPeliculas().size());
-//        Sytem.out.println("La pelicula más chida es " + Peliculas_Chidas.getPeliculas().get(random) );
+     Peliculas Peliculas_Chidas = new Peliculas();
+     
+     Peliculas_Chidas = null;
+     
+        for (int i = 0; i < Peliculas_Chidas.getPeliculas().size(); i++) {
+            System.out.println(Peliculas_Chidas.getPeliculas().get(i + 1));
+        }
+     
+        Peliculas_Chidas.addPelicula("The Others");
+        Peliculas_Chidas.deletePelicula("V for Vendetta");
+  
+        int random =  (int)(Math.random() * Peliculas_Chidas.getPeliculas().size());
+        System.out.println("La pelicula más chida es " + Peliculas_Chidas.getPeliculas().get(random) );
          
     
     }
